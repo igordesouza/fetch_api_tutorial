709f839f8d4b9acb640160f8d83217ddabe4c640
Igor de Souza <igordesouza182@gmail.com>
2023-01-06 11:45:19 -0300

    base do styles.css e index.html prontos

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+const url = "https://jsonplaceholder.typicode.com/posts"
+
+const loadingElement = document.querySelector("#loading")
+const postsContainer = document.querySelector("#posts-container")
+
