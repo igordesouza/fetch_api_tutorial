ab3bcfeea1ddc6687da2b1b40d314fceaec44851
Igor de Souza <igordesouza182@gmail.com>
2023-01-09 10:54:51 -0300

    32:56 dados do post aparecendo na pagina do post

--- js/scripts.js
+++ js/scripts.js
@@ -54,6 +54,15 @@ async function getPost(id) {
 
     loadingElement.classList.add("hide");
     postPage.classList.remove("hide");
+
+    const title = document.createElement("h1");
+    const body = document.createElement("p")
+
+    title.innerText = dataPost.title;
+    body.innerText = dataPost.body;
+
+    postContainer.appendChild(title);
+    postContainer.appendChild(body);
 }
 
 if(!postId) {
